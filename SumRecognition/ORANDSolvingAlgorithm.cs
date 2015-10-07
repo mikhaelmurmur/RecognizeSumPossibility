02bce53c3c7ca79f47740328bd2ce400365a99f0
Mikhaelmurmur <stolovich@yahoo.com>
2015-10-07 21:21:42 +0300

    test commit

---
+++ SumRecognition/ORANDSolvingAlgorithm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumRecognition
+{
+    public static class ORANDSolvingAlgorithm
+    {
+        public static string CheckNumbersForSumEquation(string firstNumber, string secondNumber, string sumNumber)
+        {
+            string result = "";
+
+            if (firstNumber.Length > sumNumber.Length || secondNumber.Length > sumNumber.Length)
+                return "The equation is not possible!!!";
+
+
+            return result;
+        }
+
+
+
+
+    }
+}
