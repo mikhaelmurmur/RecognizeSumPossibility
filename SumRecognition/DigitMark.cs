d2a24087f1b1fe6b86dac276f223bf9ad77c799c
Mikhaelmurmur <stolovich@yahoo.com>
2015-10-07 22:57:25 +0300

    Added carcase classes

---
+++ SumRecognition/DigitMark.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumRecognition
+{
+    public class DigitMark
+    {
+        public byte currentNumber { get; private set; }
+        public bool available { get; set; }
+        List<DigitMark> connections = new List<DigitMark>();
+        public DigitMark()
+        {
+            throw new InvalidOperationException("Created digit mark withour number inputed");
+        }
+
+        public DigitMark(byte number)
+        {
+            currentNumber = number;
+        }
+
+        public void AddConnection(DigitMark newConnection)
+        {
+            connections.Add(newConnection);
+        }
+
+        public void DeleteConnection(DigitMark connectionToDelete)
+        {
+            if (connections.Contains(connectionToDelete))
+            {
+                connections.Remove(connectionToDelete);
+            }
+        }
+
+        public bool ConnectedTo(DigitMark connectedNode)
+        {
+            return connections.Contains(connectedNode);
+        }
+    }
+}
---
+++ SumRecognition/DigitObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumRecognition
+{
+    class DigitObject
+    {
+        List<DigitMark> marks = new List<DigitMark>();
+        List<DigitObject> neighbours = new List<DigitObject>();
+    }
+}
